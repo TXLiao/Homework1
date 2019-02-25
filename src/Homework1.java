d0bd059640639c28a3bca995a2b97c0fc091ffd4
廖天禧 <16005037@buaa.edu.cn>
2019-02-25 17:53:45 +0800

    Initial commit

---
+++ src/Homework1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println((int)(a + b));
+    }
+}
